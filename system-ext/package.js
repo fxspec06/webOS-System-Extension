ae132fd2110b1fa90c46a48e91841b1222a83fd4
Bryan Leasot <Bryan@.(none)>
2012-08-24 12:31:02 -0400

    cleaned package.js

--- system-ext/package.js
+++ system-ext/package.js
@@ -1,21 +1,4 @@
 ﻿enyo.depends(
-	/*"system.js",
-	"keyboard.js",
-	"windows/windows.js",
-	"windows/agent.js",
-	"windows/browserAgent.js",
-	"windows/events.js",
-	"windows/manager.js",
-	"setuplogging.js",
-	"Dashboard.js",
-	"CrossAppUI.js",
-	"CrossAppResult.js",
-	"FilePicker.js"*/
-	
-	
-	// the actual dashboard...
-	//"dashboard-window",
-	
 	"windows",
 	"ApplicationEvents.js",
 	
